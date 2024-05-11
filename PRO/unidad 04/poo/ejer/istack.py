726bfdc80c6232aecf57bf733d3b1513ab1d6516
Rashi <147090292+Chugani05@users.noreply.github.com>
2024-05-11 14:50:53 +0100

    Update istack.py

--- PRO/unidad 04/poo/ejer/istack.py
+++ PRO/unidad 04/poo/ejer/istack.py
@@ -16,38 +16,44 @@ class IntegerStack:
 
     def __init__(self, *, max_size: int = 10):
         '''Utilizar atributo items para guardar los elementos'''
-        ...
+        self.max_size = max_size
+        self.items = []
 
     def push(self, item: int) -> bool:
         '''Añade item a la pila.
         Si la pila está llena retornar False, en otro caso retornar True'''
-        ...
+        if len(self.items) >= salf.max_size:
+            return False
+        self.items.insert(0, item)
+        return True
 
     def pop(self) -> int:
         '''Extraer el elemento que está en el TOP de la pila'''
-        ...
+        return self.items.pop(0)
 
     def top(self) -> int:
         '''Devolver el elemento que está en el TOP de la pila (sin extracción)'''
-        ...
+        return self.items[0]
 
     def is_empty(self) -> bool:
         '''Indica si la pila está vacía'''
-        ...
+        return len(self.items) == 0
 
     def is_full(self) -> bool:
         '''Indica si la pila está llena -> max_size'''
-        ...
+        return len(self.items) == self.max_size
 
     def expand(self, factor: int = 2) -> None:
         '''Expande el tamaño máximo de la pila en el factor indicado'''
-        ...
+        self.max_size *= factor
 
     def dump_to_file(self, path: str) -> None:
         '''Vuelca la pila a un fichero.
         - Cada item en una línea.
         - El primer elemento del fichero corresponde con el TOP de la pila.'''
-        ...
+        with open(path, 'w') as f:
+            elements = [str(n) for n in self.items]
+            f.write('\n'.join(elements))
 
     @classmethod
     def load_from_file(cls, path: str) -> IntegerStack:
@@ -56,38 +62,54 @@ class IntegerStack:
         - El primer elemento del fichero corresponde con el TOP de la pila.
         - Si la pila se llena al ir añadiendo elementos habrá que expandir con los valores
         por defecto'''
-        ...
+        result = IntegerStack()
+        content = reversed(open(path).readlines())
+        for line in content:
+            num = int(line.strip())
+            if not result.push(num):
+                result.expand()
+                result.push(num)
+        return result
 
     def __getitem__(self, index: int) -> int:
         '''Devuelve el elemento de la pila en el índice indicado'''
-        ...
+        return self.items[index]
 
     def __setitem__(self, index: int, item: int) -> None:
         '''Establece el valor de un elemento de la pila mediante el índice indicado'''
-        ...
+        self.items[index] = item 
 
     def __len__(self):
         '''Número de elementos que contiene la pila'''
-        ...
+        return len(self.items)
 
     def __str__(self):
         '''Cada elemento en una línea distinta empezando por el TOP de la pila'''
-        ...
+        return '\n'.join([str(n) for n in self.items])
+        
 
     def __add__(self, other: IntegerStack) -> IntegerStack:
         '''Sumar dos pilas.
         - La segunda pila va "encima" de la primera
         - El tamaño máximo de la pila resultante es la suma de los tamaños
         máximos de cada pila.'''
-        ...
+        new_size = self.max_size + other.max_size
+        result = IntegerStack(max_size=new_size)
+        result.items = other.items + self.items
+        return result
 
     def __iter__(self) -> IntegerStackIterator:
-        ...
+        return IntegerStackIterator(self)
 
 
 class IntegerStackIterator:
     def __init__(self, stack: IntegerStack):
-        ...
+        self.stack = stack
+        self.points = 0
 
     def __next__(self) -> int:
-        ...
+        if self.points >= len(self.stack):
+            raise StopIteration
+        num = self.stack[self.points]
+        if self.points += 1
+        return num
